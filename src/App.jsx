5908dc1fe78ff8344a31a6222e375f5d0b5368dd
Deepak Kohli <deepakkohli1810@gmail.com>
2024-10-01 07:34:43 +0530

    Added new Landing Page file

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
+import Navbar from './components/Navbar'
+import LandingPage from './components/LandingPage'
 
 function App() {
   return (
+    <>
     <div className='w-full h-screen bg-zinc-700'>
-      This is deepak 
+      <Navbar/>
+      <LandingPage/>  
     </div>
+
+
+    </>
   )
 }
 
